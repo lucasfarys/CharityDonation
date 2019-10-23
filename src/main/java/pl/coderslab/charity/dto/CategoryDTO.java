35fdbea0357f9b6656a74491be9be8d570f3128f
Lukasz Farys <lukaszfarys@gmail.com>
2019-10-23 19:25:37 +0200

    edit entity category

--- src/main/java/pl/coderslab/charity/dto/CategoryDTO.java
+++ src/main/java/pl/coderslab/charity/dto/CategoryDTO.java
@@ -3,9 +3,12 @@ package pl.coderslab.charity.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class CategoryDTO {
     Long id;
+    @NotBlank
     String name;
 }
